10de213d05163d74514e150a722b4ab1db4fc295
Ivanecky <sam.ivanecky3@gmail.com>
2021-02-13 13:04:52 -0600

    Redid the UI and added AWS integration

--- server.R
+++ server.R
@@ -10,240 +10,38 @@ library(knitr)
 library(DT)
 library(dplyr)
 library(rvest)
+library(RPostgreSQL)
+library(DBI)
+library(RSQLite)
+library(reshape2)
+library(yaml)
+library(rvest)
 
-# Function to read in webpage and produce a dataframe of scored athletes
-readPerfList = function(url){
-    # Read in webpage HTML
-    # Get HTML
-    webpage = read_html(url)
-    
-    # Read in tables
-    tables = html_table(webpage)
-    
-    # Extract first df
-    results = as.data.frame(tables[1])
-    names(results) = c("PLACE", "ATHLETE", "YEAR", "TEAM", "TIME", "MEET", "MEET.DATE")
-    
-    # Create gender column
-    results$Gender = "M"
-    
-    # Table for relays data
-    relays = data.frame()
-    
-    # Extract all the tables into dataframes
-    for( i in 2:length(tables) )
-    {
-        # Temporary df
-        temp = as.data.frame(tables[i])
-        # Make sure table has 7 columns for binding - some tables using conversions for field events have 8 columns
-        if ( ncol(temp) == 7 )
-        { 
-            # Rename and bind
-            names(temp) = c("PLACE", "ATHLETE", "YEAR", "TEAM", "TIME", "MEET", "MEET.DATE")
-            
-            # Check to assign gender - womens tables should be "even" numbers
-            if ( i%%2 == 0 ) 
-            { 
-                temp$Gender = "F" 
-            }
-            else { temp$Gender = "M" }
-            
-            results = rbind(results, temp) 
-        }
-        else if( ncol(temp) == 6)
-        {
-            relays = rbind(relays, temp)
-        }
-    }
-    
-    # Events are not tied in to data - need to add them based on times
-    results = results %>%
-        mutate(EVENT = case_when(
-            grepl('6\\.|7\\.', TIME) & (substr(TIME,1,1) %in% c('6','7')) ~ '60m', # Have to add the substr clause to get only those that start with a 6
-            grepl('21\\.|22\\.|23\\.|24\\.|25\\.|26\\.', TIME) & (substr(TIME,1,2) %in% c('21','22','23','24','25','26')) ~ '200m',
-            grepl('44\\.|45\\.|46\\.|47\\.|48\\.|49\\.|50\\.|51\\.|52\\.|53\\.|54\\.|55\\.|56\\.|57\\.|58\\.|59\\.', TIME) & 
-                (substr(TIME,1,2) %in% c('44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59')) ~ '400m',
-            grepl('1:|2:', TIME) & (substr(TIME,1,2) %in% c('1:','2:')) ~ '800m',
-            grepl('3:|4:|5:', TIME) & (substr(TIME,1,2) %in% c('3:','4:','5:')) ~ 'Mile',
-            grepl('7:|8:|9:|10:|11:', TIME) & (substr(TIME,1,2) %in% c('7:', '8:', '9:', '10', '11')) ~ '3000m',
-            grepl('13:|14:|15:|16:|17:|18:', TIME) & (substr(TIME,1,3) %in% c('13:','14:','15:','16:','17:','18:')) ~ '5000m',
-            T ~ 'OTHER'
-        ))
-    
-    # Subset to Top 50 per event
-    top50 <- results %>%
-        filter(PLACE <= 50) %>%
-        filter(EVENT %in% c('800m', 'Mile', '3000m', '5000m')) %>%
-        mutate(
-            PTS = 41 - PLACE
-        )
-    
-    # Group to one line per runner
-    pts_grp <- top50 %>%
-        group_by(ATHLETE) %>%
-        summarise(
-            TEAM = max(TEAM), 
-            YEAR = max(YEAR), 
-            GENDER = max(Gender), 
-            POINTS = sum(PTS, na.rm = T), 
-            EVENTS = n_distinct(EVENT)
-        ) %>%
-        mutate(PTS_PER_EVENT = POINTS / EVENTS) %>%
-        arrange(-POINTS)
-    
-    # Return dataframe
-    return(pts_grp)
-}
+# Read connection data from yaml
+yml <- read_yaml("aws.yaml")
 
-# Function to get the rank
-giveRank = function(df){
-    # Extract maximum total of any runner in each year
-    maxPtsCY = max(df$POINTS)
-    maxPtsPY = max(df$POINTS_PY)
-    # Extract maximum points per event for each year
-    maxPtsPerEventCY = max(df$PTS_PER_EVENT)
-    maxPtsPerEventPY = max(df$PTS_PER_EVENT_PY)
-    
-    # Create RANK value
-    df <- df %>%
-        mutate(
-            RANK = case_when(
-                    # Athlete has results from prior year
-                    maxPtsPY != 0 ~ (((POINTS / maxPtsCY) * 34) + ((POINTS_PY / maxPtsPY) * 6) +  ((PTS_PER_EVENT / maxPtsPerEventCY) * 51) + ((PTS_PER_EVENT_PY / maxPtsPerEventPY) * 9)),
-                    # Athlete has no results from prior year
-                    T ~ ((POINTS / maxPtsCY) * 40) + ((PTS_PER_EVENT / maxPtsPerEventCY) * 60))
-        ) %>%
-        arrange(-RANK) %>%
-        mutate(
-            RANK = round(RANK, 2)
-        )
-    
-    # Subset to top 100
-    df = df[1:100, ]
-    
-    # Assign overall rank
-    df$`OVERALL RANK` = 1:100
-    
-    # Return df
-    return(df)
-}
+# Connect to AWS
+# Connect to database
+aws <- dbConnect(
+    RPostgres::Postgres(),
+    host = yml$host,
+    user = yml$user,
+    password = yml$password,
+    port = yml$port
+)
 
-# Get information on athlete
-searchAthlete = function(runner){
-    
-    # Split name into first and last
-    runner = strsplit(runner, " ")[[1]]
-    
-    # Define first & last names
-    first_name = tolower(runner[1])
-    last_name = tolower(runner[2])
-    
-    # Search for the athlete
-    searchedRunner = overall[which(grepl(first_name, tolower(overall$ATHLETE)) & grepl(last_name, tolower(overall$ATHLETE))), ]
-    
-    return(searchedRunner)
-}
+# Query the data
+df <- dbGetQuery(aws, "SELECT * FROM current_power_rankings")
 
-# Get rank for each athlete
-getRank = function(runner){
-    # Split name into first and last
-    runner = strsplit(runner, " ")[[1]]
-    
-    # Define first & last names
-    first_name = tolower(runner[1])
-    last_name = tolower(runner[2])
-    
-    # Search for the athlete
-    searchedRunner = overall[which(grepl(first_name, tolower(overall$ATHLETE)) & grepl(last_name, tolower(overall$ATHLETE))), ]
-    
-    # Return rank
-    return(searchedRunner$`OVERALL RANK`)
-}
+# Drop load date
+df <- df %>% select(-c("LOAD_DATE"))
 
-# Read all of the data into one big table
-d1_cy = readPerfList('https://www.tfrrs.org/lists/3157/2020_2021_NCAA_Division_I_Indoor_Qualifying_List/2021/i')
-d1_py = readPerfList('https://www.tfrrs.org/lists/2770.html')
-d2_cy = readPerfList('https://www.tfrrs.org/lists/3158/2020_2021_NCAA_Division_II_Indoor_Qualifying_List/2021/i')
-d2_py = readPerfList('https://www.tfrrs.org/lists/2771.html')
-d3_cy = readPerfList('https://www.tfrrs.org/lists/3161/2020_2021_NCAA_Division_III_Indoor_Qualifying_List/2021/i')
-d3_py = readPerfList('https://www.tfrrs.org/lists/2772.html')
-
-# Create division columns
-d1_cy$DIVISION = "D1"
-d1_py$DIVISION = "D1"
-d2_cy$DIVISION = "D2"
-d2_py$DIVISION = "D2"
-d3_cy$DIVISION = "D3"
-d3_py$DIVISION = "D3"
-
-# Rename 2019 data
-names(d1_py) = c("ATHLETE", "TEAM", "YEAR", "GENDER", "POINTS_PY", "EVENTS_PY", "PTS_PER_EVENT_PY", "DIVISION")
-names(d2_py) = c("ATHLETE", "TEAM", "YEAR", "GENDER", "POINTS_PY", "EVENTS_PY", "PTS_PER_EVENT_PY", "DIVISION")
-names(d3_py) = c("ATHLETE", "TEAM", "YEAR", "GENDER", "POINTS_PY", "EVENTS_PY", "PTS_PER_EVENT_PY", "DIVISION")
-
-# Select only required columns from 2019 data
-d1_py = d1_py %>% select(ATHLETE, POINTS_PY, EVENTS_PY, PTS_PER_EVENT_PY)
-d2_py = d2_py %>% select(ATHLETE, POINTS_PY, EVENTS_PY, PTS_PER_EVENT_PY)
-d3_py = d3_py %>% select(ATHLETE, POINTS_PY, EVENTS_PY, PTS_PER_EVENT_PY)
-
-# Join divisional data together
-d1 = d1_cy %>% left_join(d1_py, by = c("ATHLETE"))
-d2 = d2_cy %>% left_join(d2_py, by = c("ATHLETE"))
-d3 = d3_cy %>% left_join(d3_py, by = c("ATHLETE"))
-
-
-# Function to handle NAs
-handleNA = function(df)
-{
-    df = df %>%
-        mutate(
-            POINTS_PY = ifelse(is.na(POINTS_PY), 0, POINTS_PY),
-            EVENTS_PY = ifelse(is.na(EVENTS_PY), 0, EVENTS_PY),
-            PTS_PER_EVENT_PY = ifelse(is.na(PTS_PER_EVENT_PY), 0, PTS_PER_EVENT_PY)
-        ) %>%
-        mutate(
-            PTS_PER_EVENT = round(PTS_PER_EVENT, 2),
-            PTS_PER_EVENT_PY = round(PTS_PER_EVENT_PY, 2)
-        )
-    # Return data frame
-    return(df)
-}
-
-# Handle all the NAs
-d1 = handleNA(d1)
-d2 = handleNA(d2)
-d3 = handleNA(d3)
+# Rename columns
+colnames(df) <- c("Athlete", "Team", "Class", "Gender", "Total Points", "Events", "Points Per Event", "Preliminary Rank Score", "Final Rank Score", "Overall Power Ranking", "Event Year", "Division")
 
 # Split by gender
-menD1 = d1 %>% filter(GENDER == 'M')
-womenD1 = d1 %>% filter(GENDER == 'F')
-
-menD2 = d2 %>% filter(GENDER == 'M')
-womenD2 = d2 %>% filter(GENDER == 'F')
-
-menD3 = d3 %>% filter(GENDER == 'M')
-womenD3 = d3 %>% filter(GENDER == 'F')
-
-# Run ranking function against data
-menD1 = giveRank(menD1)
-womenD1 = giveRank(womenD1)
-
-menD2 = giveRank(menD2)
-womenD2 = giveRank(womenD2)
-
-menD3 = giveRank(menD3)
-womenD3 = giveRank(womenD3)
-
-# Combine all the data from separate divisions
-men = rbind(menD1, menD2, menD3)
-women = rbind(womenD1, womenD2, womenD3)
-
-# Create overall data set
-overall = rbind(men, women)
-
-# Rename
-colnames(men) <- c("Athlete", "Team", "Year", "Gender", "Total Points", "Total Events", "Points Per Event", "DIVISION", "Points (Last Yr)", "Events (Last Yr.)", "Points Per Event (Last Yr.)", "Rank Score", "Overall Ranking")
-colnames(women) <- c("Athlete", "Team", "Year", "Gender", "Total Points", "Total Events", "Points Per Event", "DIVISION", "Points (Last Yr)", "Events (Last Yr.)", "Points Per Event (Last Yr.)", "Rank Score", "Overall Ranking")
+men <- df %>% filter(Gender == 'M')
+women <- df %>% filter(Gender == 'F')
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -252,19 +50,19 @@ shinyServer(function(input, output) {
     mensData = reactive({
         
         # Temporarily set data
-        df = men %>% filter(DIVISION == "D1")
+        df = men %>% filter(Division == "D1")
         
         if ( input$division == "D1" )
         {
-            df = men %>% filter(DIVISION == "D1")
+            df = men %>% filter(Division == "D1")
         }
         else if ( input$division == "D2" )
         {
-            df = men %>% filter(DIVISION == "D2")
+            df = men %>% filter(Division == "D2")
         }
         else if ( input$division == "D3" )
         {
-            df = men %>% filter(DIVISION == "D3")
+            df = men %>% filter(Division == "D3")
         }
         
         return(df)
@@ -273,191 +71,27 @@ shinyServer(function(input, output) {
     womensData = reactive({
         
         # Temporarily set data
-        df = women %>% filter(DIVISION == "D1")
+        df = women %>% filter(Division == "D1")
         
         if ( input$division == "D1" )
         {
-            df = women %>% filter(DIVISION == "D1")
+            df = women %>% filter(Division == "D1")
         }
         else if ( input$division == "D2" )
         {
-            df = women %>% filter(DIVISION == "D2")
+            df = women %>% filter(Division == "D2")
         }
         else if ( input$division == "D3" )
         {
-            df = women %>% filter(DIVISION == "D3")
+            df = women %>% filter(Division == "D3")
         }
         
         return(df)
     })
     
-    output$menRank = renderTable(mensData())
+    output$menRank = renderDataTable(mensData())
     
-    output$womenRank = renderTable(womensData())
-    
-    # Functions to pull out runner info
-    # Get name of first runner
-    name1 = reactive({
-        input$compareButton
-        # Get the runner
-        runner1 = searchAthlete(input$runnerName1)
-        
-        # Pull out the name
-        runnerName1 = runner1$Athlete
-    })
-    
-    # Get name of first runner
-    name2 = reactive({
-        input$compareButton
-        # Get the runner
-        runner2 = searchAthlete(input$runnerName2)
-        
-        # Pull out the name
-        runnerName2 = runner2$Athlete
-    })
-    
-    # Get the overall rank
-    rank1 = reactive({
-        input$compareButton
-        # Pull out the name
-        runnerRank1 = getRank(input$runnerName1)
-    })
-    
-    # Get the overall rank
-    rank2 = reactive({
-        input$compareButton
-        # Pull out the name
-        runnerRank2 = getRank(input$runnerName2)
-    })
-    
-    # Get the 2020 points
-    pts1 = reactive({
-        input$compareButton
-        # Get the runner
-        runner1 = searchAthlete(input$runnerName1)
-        
-        # Pull out the name
-        runnerPts1 = runner1$`Total Points`
-    })
-    
-    # Get the overall rank
-    pts2 = reactive({
-        input$compareButton
-        # Get the runner
-        runner2 = searchAthlete(input$runnerName2)
-        
-        # Pull out the name
-        runnerPts2 = runner2$`Total Points`
-    })
-    
-    # Get the points per event in 2020
-    ptsPerEvent1 = reactive({
-        input$compareButton
-        # Get the runner
-        runner1 = searchAthlete(input$runnerName1)
-        
-        # Pull out the name
-        runnerPtsPerEvent1 = runner1$`Points Per Event`
-    })
-    
-    # Get the overall rank
-    ptsPerEvent2 = reactive({
-        input$compareButton
-        # Get the runner
-        runner2 = searchAthlete(input$runnerName2)
-        
-        # Pull out the name
-        runnerPtsPerEvent2 = runner2$`Points Per Event`
-    })
-    
-    # Functions to output the value boxes
-    # Runner 1 Name
-    output$runnerName1 <- renderValueBox({
-        # Run on button press
-        input$compareButton
-        # Generate box
-        valueBox(
-            value = paste0(isolate(name1())),
-            subtitle = "Name"
-        )
-    })
-    
-    # Runner 2 Name
-    output$runnerName2 <- renderValueBox({
-        # Run on button press
-        input$compareButton
-        # Generate box
-        valueBox(
-            value = paste0(isolate(name2())),
-            subtitle = "Name"
-        )
-    })
-    
-    # Runner 1 Rank
-    output$runnerRank1 <- renderValueBox({
-        # Run on button press
-        input$compareButton
-        # Generate box
-        valueBox(
-            value = paste0(isolate(rank1())),
-            subtitle = "Rank"
-        )
-    })
-    
-    # Runner 2 Rank
-    output$runnerRank2 <- renderValueBox({
-        # Run on button press
-        input$compareButton
-        # Generate box
-        valueBox(
-            value = paste0(isolate(rank2())),
-            subtitle = "Rank"
-        )
-    })
-    
-    # Runner 1 Points
-    output$runnerPts1 <- renderValueBox({
-        # Run on button press
-        input$compareButton
-        # Generate box
-        valueBox(
-            value = paste0(isolate(pts1())),
-            subtitle = "Current Year Points"
-        )
-    })
-    
-    # Runner 2 Points
-    output$runnerPts2 <- renderValueBox({
-        # Run on button press
-        input$compareButton
-        # Generate box
-        valueBox(
-            value = paste0(isolate(pts2())),
-            subtitle = "Current Year Points"
-        )
-    })
-    
-    # Runner 1 Pts Per Event
-    output$runnerPtsPerEvent1 <- renderValueBox({
-        # Run on button press
-        input$compareButton
-        # Generate box
-        valueBox(
-            value = paste0(isolate(ptsPerEvent1())),
-            subtitle = "Current Year Points Per Event"
-        )
-    })
-    
-    # Runner 2 Points Per Event
-    output$runnerPtsPerEvent2 <- renderValueBox({
-        # Run on button press
-        input$compareButton
-        # Generate box
-        valueBox(
-            value = paste0(isolate(ptsPerEvent2())),
-            subtitle = "Current Year Points Per Event"
-        )
-    })
+    output$womenRank = renderDataTable(womensData())
     
 })
 
--- ui.R
+++ ui.R
@@ -14,11 +14,6 @@ library(png)
 ui = dashboardPage(
     dashboardHeader(title = "NCAA Indoor Track Power Rankings"),
     dashboardSidebar(
-        # Search boxes for athletes to compare
-        textInput("runnerName1", "Runner 1", "Weini Kelati"),
-        textInput("runnerName2", "Runner 2", "Edwin Kurgat"),
-        # Search button
-        actionButton("compareButton", "Compare Runners", style = "color: #000000; background-color: #42f44e; border-color: #2e6da4"),
         # Select Division for data
         selectInput("division", 
                     "Select Division",
@@ -26,7 +21,7 @@ ui = dashboardPage(
         # Sidebar tab menu
         sidebarMenu(
             menuItem("Power Rankings", tabName = "powerRankings"),
-            menuItem("Runner Comparison", tabName = "runnerCompare")
+            menuItem("About", tabName = "about")
         )
     ),
     # Define body of application
@@ -36,34 +31,47 @@ ui = dashboardPage(
             # Main page with rankings
             tabItem("powerRankings", 
                 fluidRow(
+                  h1("NCAA Indoor Track Power Rankings"),
+                  p("Please read the 'About' page to answer questions.")
+                ),
+                fluidRow(
                     column(12,
                            h1("Mens Rankings"),
-                           tableOutput('menRank')
+                           dataTableOutput('menRank')
                     )
                 ),
                 fluidRow(
                     column(12,
                            h1("Womens Rankings"),
-                           tableOutput('womenRank')
+                           dataTableOutput ('womenRank')
                     )
                 )
             ),
-            # Second page for runner compare tool
-            tabItem("runnerCompare",
-                # Athlete 1
-                fluidRow(
-                    valueBoxOutput("runnerName1", width = 3),
-                    valueBoxOutput("runnerRank1", width = 3),
-                    valueBoxOutput("runnerPts1", width = 3),
-                    valueBoxOutput("runnerPtsPerEvent1", width = 3)
-                ),
-                # Athlete 2
-                fluidRow(
-                    valueBoxOutput("runnerName2", width = 3),
-                    valueBoxOutput("runnerRank2", width = 3),
-                    valueBoxOutput("runnerPts2", width = 3),
-                    valueBoxOutput("runnerPtsPerEvent2", width = 3)
-                )
+            # About page
+            tabItem("about",
+                h1("Welcome to the NCAA Indoor Track Power Rankings"),
+                    h2("How does it work?"),
+                        p(
+                            "In short, we calculate scores for athletes based on a number of factors. Performances are evaluated based on how an athlete sits on the indoor performance list, how strong their time is relative to others in that event, and their strength across multiple events.
+                            An athlete accumulates points on all of these metrics, with a maximum of 200 point per event. After weighting a number of developed metrics, we end up with a `RANK` value which is their final score, and determines their overall power ranking."
+                            ),
+                    h2("Can you explain the different columns? There are a lot of numbers..."),
+                        p("Sure. It can be a little confusing so here is an explanation of our developed metrics:"),
+                        p("Total Points: The total number of points the ranking system gave an athlete based on their standing in the NCAA and their specific time."),
+                        p("Events: Number of events an athlete is in the NCAA Top 100 for at the moment."),
+                        p("Points Per Event: The average number of points (out of 200) an athlete has for all their events."),
+                        p("Preliminary Rank Score: Our first scoring iteration done. Won't give away the full details for this but it helps us for our next metric."),
+                        p("Final Rank Score: The final ranking metric we use to order the athletes. Developed using the Preliminary Rank Score."),
+                        p("Overall Power Ranking: Where the athlete sits in their respective Division and Gender after ordering by our Final Rank Score."),
+                    h2("Why aren't all the athletes in the NCAA in the lists?"),
+                        p("In short - data limitations. Read more below."),
+                    h2("Some of your rankings seem...bad."),
+                        p("Well, that's not a question but it's a fair point. We are in the early stages of developing these (automated) rankings which means 1. There's a lot of improvements to be made and 2. We don't make manual 'corrections', what the algorithms develop is what you get.
+                          No matter how good we tune them, there will ALWAYS be errors. The goal is that with more time and work, we can really dial these in. Of course, if you are upset about a ranking, just remember, blame the computer, not us."),
+                    h2("What comes next?"),
+                        p("We've mentioned things about our next steps already but there are a bunch of future adaptations that we'd love to build. Creating better algorithms, getting more (all) athletes into our system, adding more features like comparing runners, schools, etc. Maybe even doing 
+                          cross country and/or outdoor track. There are plenty of opportunities to make this much better and this is essentially a 'beta' version. We wanted to get it public so you can check it out, maybe use it, and provide feedback on the good, bad, and otherwise. If you have 
+                          comments, suggestions, or find bugs, please reach out and let us know. You can find us on Instagram at @run.the.numbers or @ivanxcky.")
             )
         )
     )
